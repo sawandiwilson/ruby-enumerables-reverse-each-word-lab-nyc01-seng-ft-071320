5c1eccfd23312f67e987af3cd9b6a92153d403ab
Sawandi Wilson <sawandiwilson@gmail.com>
2020-07-08 15:48:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
-def reverse_each_word("Hello there, and how are you?")
+def reverse_each_word(str)
+  :sentence1) { "Hello there, and how are you?" }
    revered = sentence("").collect do |word|
      word.reverse
 end
